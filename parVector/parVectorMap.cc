6f36220185547d1f84a11c616ec372a26303844f
brunowu <xinzhe.wu1990@gmail.com>
2018-02-25 22:16:07 +0100

    c++ parvec construct

---
+++ parVector/parVectorMap.cc
@@ -0,0 +1,83 @@
+#include <pVecMap.h>
+
+template<typename T, typename S>
+parVectorMap<T,S>::parVectorMap(MPI_Comm ncomm, S lbound, S ubound)
+{
+	MPI_Comm_dup(ncomm, &comm);
+	MPI_Comm_size(comm, &nproc);
+	MPI_Comm_rank(comm, &rank);
+
+	//setting lower and upper bound
+	lower_bound = lbound;
+	upper_bound = ubound;
+	//get local vector size
+	local_size = upper_bound - lower_bound;
+	//initial global size
+	global_size = 0;
+
+	lprocbound_map = NULL;
+	uprocbound_map = NULL;
+
+	{
+		// Allocate process boundary maps to number of processes
+		lprocbound_map = new int[nproc] ; 
+		uprocbound_map = new int [nproc] ;
+		// Gather all lower bounds and all upper bounds
+		MPI_Allgather(&lower_bound , 1 , MPI_INT , lprocbound_map , 1 , MPI_INT, comm ) ;
+		MPI_Allgather(&upper_bound , 1 , MPI_INT , uprocbound_map , 1 , MPI_INT , comm ) ;
+		// Find global size and set vectormap to describe upper - bounds
+
+		for (int i=0; i<nproc; i++) {
+			vectormap [i] = uprocbound_map [i] ;
+			if ( uprocbound_map [ i ] > global_size ) {
+				global_size = uprocbound_map[i]; 
+			}
+	        }
+	
+	}
+ 
+	loctot_size = local_size;
+}
+
+template<typename T, typename S>
+parVectorMap<T,S>::~parVectorMap(){
+	MPI_Comm_free(&comm);
+	if(lprocbound_map!=NULL){
+		delete [] lrpocbound_map;
+	}
+	
+        if(uprocbound_map!=NULL){
+                delete [] urpocbound_map;
+        }
+}
+
+//convert local index to gobal or vice versa
+template<typename T, typename S>
+S parVectorMap<T,S>::Loc2Glob(S local_index){
+	if((local_index < global_size) && (local_index >= 0))
+		{return lower_bound + local_index;}
+	else
+		{return -1;}
+}
+
+template<typename T, typename S> 
+S parVectorMap<T,S>::Glob2Loc(S global_index){
+	if((global_index >= local_size) && (global_index < upper_bound))
+		{return global_index - lower_bound;}
+}
+
+//get
+template<typename T, typename S>
+int parVectorMap<T,S>::GetOwer(S index)
+{
+	if((index < global_size) && (index >= 0)){
+		for (int i = 0; i <nproc; i++){
+			if(index < uprocbound_map[i]){return i;}
+		}
+	}
+	else {return -1;}
+
+}
+
+
+
---
+++ parVector/parVectorMap.h
@@ -0,0 +1,60 @@
+#ifndef __PARVECTORMAP_H__
+#define __PARVECTORMAP_H__
+
+#include <mpi.h>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+template<typename T, typename S>
+class parVectorMap
+{
+	private:
+		MPI_Comm  comm;
+		int	  nproc;
+		int	  rank;
+
+		S	  low_bound;
+		S	  up_bound;
+	
+		S	  local_size;
+		S	  loctot_size;
+		S         global_size;
+
+		std::map<int,S> vectormap;
+		int	  *lprocbound_map, *uprocbound_map;
+
+		std::map<S,S> loc2glob;
+		std::map<S,S> glob2loc;
+
+		int users;
+
+
+	public:
+		//constructor
+		parVectorMap(MPI_Comm ncomm, S lbound, S ubound);
+		//destroyer
+		~pVecMap();
+
+		S Loc2Glob(S local_index);
+		S Glob2Loc(S global_index);
+
+		//get
+		int GetOwner(S index);
+		int GetRank(){return rank;};
+
+		S GetLowerBound(){return lower_bound};
+		S GetUpperBound(){return upper_bound};
+		S GetLocalSize(){return local_size;};
+		S GetGlobalSize(){return global_size};
+		S GetLocTotSize(){return loctot_size};
+
+		//Active usrs
+
+		int AddUser(){users++; return users;};
+		int DeleteUser(){users--;return users;};
+		int GetUser(){return users;};
+};
+
+#endif
