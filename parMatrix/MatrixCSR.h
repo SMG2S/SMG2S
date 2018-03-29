9a64df92d5e4643a2bb0f2cca477b32ae613bf8f
brunowu <xinzhe.wu1990@gmail.com>
2018-03-29 22:41:53 +0200

    CSR Sparse matrix addition

--- parMatrix/MatrixCSR.h
+++ parMatrix/MatrixCSR.h
@@ -5,6 +5,7 @@ struct MatrixCSR
 {
 	S	nrows;
 	S	nnz;
+	S   ncols;
 
 	std::vector<S> rows;
 	std::vector<S> cols;
@@ -22,6 +23,9 @@ struct MatrixCSR
 	{
 	
 		nnz = nnz_in;
+		ncols = nrows_in;
+		nrows = nrows_in;
+
 		rows.reserve(nrows_in+1);
 		cols.reserve(nnz);
 		vals.reserve(nnz);	
@@ -36,6 +40,80 @@ struct MatrixCSR
 			vals.empty();
 		}
 	};
+
+	T GetValue(S row, S col)
+	{
+		T val;
+		S currCol;
+
+		for(S pos = rows[row - 1] - 1; pos < rows[row] - 1; ++pos){
+			currCol = cols[pos];
+			if (currCol == col){
+				val = vals[pos];
+			} else{
+				val = 0.0;
+			}
+		}
+		return val;
+	};
+
+
+	void SetValue(S row, S col, T val)
+	{
+		S currCol;
+
+		for(S pos = rows[row - 1] - 1; pos < rows[row] - 1; ++pos){
+			currCol = cols[pos];
+			if (currCol == col){
+				vals[pos] = val;
+			} 
+		}
+	};
+
+	void InsertValue(S row, S col, T val)
+	{
+		S currCol, prevCol;
+		typename std::vector<S>::iterator it;
+		typename std::vector<T>::iterator itv;
+
+		for(S pos = rows[row - 1] - 1; pos < rows[row] - 1; ++pos){
+			currCol = cols[pos];
+			prevCol = cols[pos-1];
+			if (currCol == col){
+				SetValue(row, col, val);
+			} else if((currCol > col) && (prevCol < col)){
+				it = currCol;
+				itv = currCol;
+				cols.insert(it,col);
+				vals.insert(itv, val);
+				nnz++;
+				return;
+			}
+		}
+	};
+
+	void Add(MatrixCSR<T,S> m){
+		S row, nnz;
+		T v1, v2, v;
+		
+		if (nrows != m.nrows || ncols != m.ncols) {
+			printf("Cannot add: matrices dimensions don't match.\n");
+			return;
+		}
+
+		for (S i = 1; i <= nrows; i++){
+			for (S j = 1; j <= ncols; j++) {
+				v1 = GetValue(i,j);
+				v2 = m->GetValue(i,j);
+				v = v1 + v2;
+				if(v != 0.0){
+					InsertValue(i,j,v1+v2);
+				}
+			}
+		}
+
+	};
+
 
 	void Free()
 	{
