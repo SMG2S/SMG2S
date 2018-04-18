2e2f5aab8a01ca37a2e3ec0d5b8f59838aaac3ef
brunowu <xinzhe.wu1990@gmail.com>
2018-04-18 09:02:58 +0200

    data processing and graphing

---
+++ verification/traitement.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+grep "@> The eigenvalue" vector1_results.txt > tmp.txt
+
+awk '{print $5 " " $7 }' tmp.txt > tmp2.txt
+
+awk '{print substr($0, 1, length($0)-1)}' tmp2.txt > vector1_results_clean.txt
+
+#awk '{for (NF=3;i<=100;NF++) print $2 " " $3}' vector1.txt > vector1_initial_clean.txt
+
+rm tmp.txt tmp2.txt
+
+tail -n+3 vector1.txt > tmp2.txt
+
+awk '{print $2 " " $3}' tmp2.txt > vector1_initial_clean.txt
+
+rm tmp2.txt
+
+#awk 'NR>2' vector1.txt > tmp2.txt
+
+
